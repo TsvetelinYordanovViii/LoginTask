86f72a0717a5858ec47fa8448bc018ab6c05dbdf
Tsvetelin Yordanov <tsvetelin_yordanov_viii@abv.bg>
2023-08-04 17:02:35 +0300

    Final commit for the day. Added some functions and some styling rules to use on Monday.

--- Scripts/LoginPageScripts.js
+++ Scripts/LoginPageScripts.js
@@ -2,3 +2,12 @@ $("#register-option").click(() => {
     $("#login").addClass("slide-out-left");
     $("#register").addClass("slide-in-left");
 });
+
+
+$("#login-btn").click(() => {
+
+})
+
+$("#register-btn").click(() => {
+
+})
