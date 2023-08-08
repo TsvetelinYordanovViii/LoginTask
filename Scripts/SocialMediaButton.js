52921bd580b3f7cc899e1135bcdc4799c4e03a23
Tsvetelin Yordanov <tsvetelin_yordanov_viii@abv.bg>
2023-08-08 11:34:41 +0300

    Added a js file to add a class to the icon and text which will are used in the scss file to color them appropriately.

---
+++ Scripts/SocialMediaButton.js
@@ -0,0 +1,32 @@
+$('#facebook').hover(() => {
+    $('#facebook .social-media-name').addClass('hoveredOver');
+    $('#facebook .social-media-icon').addClass('hoveredOver');
+}, () => {
+    $('#facebook .social-media-name').removeClass('hoveredOver');
+    $('#facebook .social-media-icon').removeClass('hoveredOver');
+})
+
+$('#twitter').hover(() => {
+    $('#twitter .social-media-name').addClass('hoveredOver');
+    $('#twitter .social-media-icon').addClass('hoveredOver');
+}, () => {
+    $('#twitter .social-media-name').removeClass('hoveredOver');
+    $('#twitter .social-media-icon').removeClass('hoveredOver');
+})
+
+$('#instagram').hover(() => {
+    $('#instagram .social-media-name').addClass('hoveredOver');
+    $('#instagram .social-media-icon').addClass('hoveredOver');
+}, () => {
+    $('#instagram .social-media-name').removeClass('hoveredOver');
+    $('#instagram .social-media-icon').removeClass('hoveredOver');
+})
+
+$('#youtube').hover(() => {
+    $('#youtube .social-media-name').addClass('hoveredOver');
+    $('#youtube .social-media-icon').addClass('hoveredOver');
+}, () => {
+    $('#youtube .social-media-name').removeClass('hoveredOver');
+    $('#youtube .social-media-icon').removeClass('hoveredOver');
+})
+
